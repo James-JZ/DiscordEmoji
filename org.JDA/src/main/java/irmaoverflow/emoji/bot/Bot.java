fd4001b7d0c4a36356f61e9d3f55f46b2b5d69a3
James-JZ <40481395+James-JZ@users.noreply.github.com>
2018-09-15 19:09:37 -0400

    status

--- org.JDA/src/main/java/irmaoverflow/emoji/bot/Bot.java
+++ org.JDA/src/main/java/irmaoverflow/emoji/bot/Bot.java
@@ -3,18 +3,57 @@ package irmaoverflow.emoji.bot;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class Bot {
+public class Bot extends ListenerAdapter
+{
     final static String token = "NDkwNTY1MTA0OTIwOTUyODUz.Dn7J7w.EzVC6LeXSKsiuX2vbXipzat9zQ0";
+    final static String prefix = "!";
+    public static void main(String args[]) throws Exception
+    {
 
-    public static void main(String args[]) {
+        TextChannel channel = new evt.getChannel();
+        TextChannel test =
+
+
         try {
-            JDA jda = new JDABuilder(AccountType.BOT).setToken(token).buildBlocking();
+            JDA jda = new JDABuilder(AccountType.BOT)
+                    .setToken(token)
+                    .buildBlocking();
+                jda.setAutoReconnect(true);
+                jda.addEventListener(new Bot());
+                System.out.print("Hello");
+
+
         }
         catch(Exception e) {
             System.out.println("Token invalid");
         }
 
         System.out.println("Token Loaded");
+
+    }
+    public void OnMessageReceived(MessageReceivedEvent evt) {
+        // Objects
+        User objUser = evt.getAuthor();
+        MessageChannel objMsgCh = evt.getChannel();
+        Message objMsg = evt.getMessage();
+        System.out.print("Print something");
+        // Commmands
+        if (objMsg.getContentRaw().equalsIgnoreCase(prefix + "ping"))
+        {
+
+            objMsgCh.sendMessage(objUser.getAsMention() + "Pong").queue();
+
+        }
+        else{
+            objMsgCh.sendMessage(objUser.getAsMention() + "This is incorrect"). queue();
+        }
+
     }
 }
